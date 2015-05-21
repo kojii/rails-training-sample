d44c951396c2c32ede98be182e318dad2e5ff8f6
Koji Saito <saito@grepfind.co>
2015-05-21 12:07:38 +0900

    カラム名を変更
    あと、コメントも追加

--- db/migrate/20150519012610_create_books.rb
+++ db/migrate/20150519012610_create_books.rb
@@ -1,13 +1,13 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.integer :book_id
-      t.string :title, null: false
-      t.string :author
-      t.string :publisher
-      t.date   :regist_date
+      t.integer :book_id           # ISBNなどの識別子
+      t.string :title, null: false # タイトル
+      t.string :author             # 著者
+      t.string :publisher          # 出版社
+      t.date   :publish_date       # 出版日
 
-      t.timestamps
+      t.timestamp                  # レコード作成日時と更新日時が作成される
     end
   end
 end
